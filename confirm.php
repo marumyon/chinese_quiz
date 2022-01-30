85eba23322cff3927b0e80f80c8bd8cd811f2787
marumyon <yumenoxx0313@gmail.com>
2022-01-30 15:30:41 +0900

    test

--- confirm.php
+++ confirm.php
@@ -1,26 +1,30 @@
 <?php
 require_once('./quiz.php');
-session_start();
+                   session_start();
 
 //ユーザーの答えを受け取る
-$userAnswer = htmlspecialchars($_POST['answer'], ENT_QUOTES, 'UTF-8');
+$userAnswer = 
+htmlspecialchars($_POST['answer'], 
+ENT_QUOTES, 'UTF-8');
 
 //ユーザーの答えを配列にいれてセッションで管理
 if (isset($_SESSION['userAnswers'])) {
    array_push($_SESSION['userAnswers'], $userAnswer);
-} else {
+                                } else {
    $_SESSION['userAnswers'] = [$userAnswer];
 }
 
 $quiz = new Quiz($_SESSION['pageNum'] - 1);
  
 // 質問数終わったら解答ページへ
-if ($quiz->nextQuiz()) 
-{
+if ($quiz->nextQuiz())                                     {
    $_SESSION['pageNum']++;
    header("Location:index.php");
    exit;
-} else {
+} else 
+
+
+{
    header("Location:result.php");
    exit;
 }
